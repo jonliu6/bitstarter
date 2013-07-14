e2306c7132645057cc48cf33da1da2388522b930
Ubuntu12 <jonliu6@ubuntu.(none)>
2013-07-14 15:59:22 -0700

    test

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var fname = "index.html";
+// var fname = "hello.htm";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(fname) );
+  response.send(fs.readFileSync(fname).toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
